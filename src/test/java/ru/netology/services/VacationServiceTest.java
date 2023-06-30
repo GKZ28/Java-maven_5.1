8647e8901628dae75e32e1d37d14cd08db66ada1
gkz <giorgadzekonstantin@gmail.com>
2023-06-30 15:26:24 +0400

    Added Parameterized Test

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -24,17 +24,25 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
-    @Test
-    public void testCalculateVacationMonths1() {
-        VacationService vacationService = new VacationService();
-        int months = vacationService.calculateVacationMonths(10_000, 3_000, 20_000);
-    }
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000",
+            "100000, 60000, 150000"
+    })
 
-    @Test
-    public void testCalculateVacationMonths2() {
+    public void testCalculateVacationMonths1(int income, int expense, int threshold) {
         VacationService vacationService = new VacationService();
-        int months = vacationService.calculateVacationMonths(100_000, 60_000, 150_000);
+        int months = vacationService.calculateVacationMonths( income, expense, threshold);
     }
 }
+
+//    @Test
+//    public void testCalculateVacationMonths2() {
+//        VacationService vacationService = new VacationService();
+//        int months = vacationService.calculateVacationMonths(100_000, 60_000, 150_000);
+//    }
+//}
